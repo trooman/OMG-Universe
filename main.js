d17a5184ab68d472eec5012b45216c6c14de5000
trooman <52906690+trooman@users.noreply.github.com>
2020-03-16 17:56:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let closeButton = document.querySelector('#closeButton');
+
+button.addEventListener('click', function () {
+  modal.classList.add('modal-active');
+  menu.classList.remove('header-navbar__navigation__active');
+});
+
+closeButton.addEventListener('click', function () {
+  modal.classList.remove('modal-active');
+});
+
+// бургер меню
+let menuButton = document.querySelector('.menu-button');
+let menu = document.querySelector('.header-navbar__navigation');
+
+menuButton.addEventListener('click', function () {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-navbar__navigation__active');
+});
