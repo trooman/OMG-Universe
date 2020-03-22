a09939144911adf5b7de77571b505206fb13013c
trooman <52906690+trooman@users.noreply.github.com>
2020-03-22 22:59:51 +0200

    Added pictures + adaptive

--- js/main.js
+++ js/main.js
@@ -54,16 +54,101 @@ findUs.onclick = function () {
 }
 
 
-// show-hide picture
+// Open/Close OMG Image
+// let imgLink = document.querySelectorAll('.services-img-link');
+// for (let i = 0; i < imgLink.length; i++) {
+//   imgLink[i].onclick = function () {
 
-// let imgLink = document.querySelector('.services-img-link');
-// let imgs = document.querySelector('.services-img');
-
-// function showHide() {
-//   if (imgs.style.display = 'none') {
-//     imgs.style.display = 'block';
-//   } else {
-//     imgs.style.display = 'none';
 //   }
 // }
-// imgLink.onclick = showHide();
+
+let openButton = document.querySelector('.services-img-link'),
+  popup = document.querySelector('.popup'),
+  close = document.querySelector('.close');
+
+openButton.onclick = function () {
+  popup.style.display = 'block';
+}
+
+close.onclick = function () {
+  popup.style.display = 'none';
+}
+
+// window.onclick = function (e) {
+//   if (e.target == popup) popup.style.display = 'none';
+// }
+
+
+
+
+let openButton2 = document.querySelector('.services-img-link2'),
+  popup2 = document.querySelector('.popup2'),
+  close2 = document.querySelector('.close2');
+
+openButton2.onclick = function () {
+  popup2.style.display = 'block';
+}
+
+close2.onclick = function () {
+  popup2.style.display = 'none';
+}
+
+
+
+
+
+let openButton3 = document.querySelector('.services-img-link3'),
+  popup3 = document.querySelector('.popup3'),
+  close3 = document.querySelector('.close3');
+
+openButton3.onclick = function () {
+  popup3.style.display = 'block';
+}
+
+close3.onclick = function () {
+  popup3.style.display = 'none';
+}
+
+
+
+
+let openButton4 = document.querySelector('.services-img-link4'),
+  popup4 = document.querySelector('.popup4'),
+  close4 = document.querySelector('.close4');
+
+openButton4.onclick = function () {
+  popup4.style.display = 'block';
+}
+
+close4.onclick = function () {
+  popup4.style.display = 'none';
+}
+
+
+
+
+let openButton5 = document.querySelector('.services-img-link5'),
+  popup5 = document.querySelector('.popup5'),
+  close5 = document.querySelector('.close5');
+
+openButton5.onclick = function () {
+  popup5.style.display = 'block';
+}
+
+close5.onclick = function () {
+  popup5.style.display = 'none';
+}
+
+
+
+let openButton6 = document.querySelector('.services-img-link6'),
+  popup6 = document.querySelector('.popup6'),
+  close6 = document.querySelector('.close6');
+
+openButton6.onclick = function () {
+  popup4.style.display = 'block';
+}
+
+close6.onclick = function () {
+  popup6.style.display = 'none';
+}
